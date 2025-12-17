dfcdc1fe25ee047d6983869c21c3ae972cb1717b
Thanapol <Thanapol.Sopanhari@7peakssoftware.com>
2025-12-17 16:49:25 +0700

    update API query for each fruit chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://api.nal.usda.gov/fdc/v1/foods/list?api_key=DEMO_KEY")
+        
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=" + fruit_chosen)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
